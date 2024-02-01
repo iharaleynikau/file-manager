e7c07438bcfefcd54fb0662569f6da85b04c17d4
Igor Aleynikov <iharaleynikov@gmail.com>
2024-02-01 12:46:47 +0300

    ls: list correct sorting

--- src/commands/ls.js
+++ src/commands/ls.js
@@ -13,7 +13,6 @@ const ls = async () => {
       stat(path + listItem, (err, stats) => {
         resolve({
           name: listItem,
-          index: list.indexOf(listItem),
           type: stats.isFile() ? 'file' : 'directory'
         });
       });
@@ -22,11 +21,31 @@ const ls = async () => {
     listData.add(listItemData);
   }
 
-  const result = await Promise.allSettled(listData);
+  const resultPromise = await Promise.allSettled(listData);
 
-  return result.map(data => {
+  const resultToSort = resultPromise.map(data => {
     return data.value;
   });
+
+  const sortByFiles = resultToSort.filter(item => {
+    return item.type === 'file';
+  });
+
+  const sortByDirs = resultToSort.filter(item => {
+    return item.type === 'directory';
+  });
+
+  sortByFiles.sort((a, b) => a.name.localeCompare(a.name));
+  sortByDirs.sort((a, b) => a.name.localeCompare(a.name));
+
+  const result = [...sortByDirs, ...sortByFiles].map((item, index) => {
+    return {
+      ...item,
+      index
+    };
+  });
+
+  return result;
 };
 
 export default ls;
